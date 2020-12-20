b416fa9b5b3c01741ff5fd4c77e71a619b20d12e
Martin Grotz <martin.grotz@gmx.de>
2020-12-20 22:24:06 +0100

    Basic version with DH11 temp sensor reading capability

---
+++ lib/hello_nerves.ex
@@ -0,0 +1,19 @@
+defmodule TemperatureBabyphone do
+
+  @moduledoc """
+  Documentation for TemperatureBabyphone.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TemperatureBabyphone.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rpi4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export MIX_TARGET=rpi4 && mix firmware && ./upload.sh
